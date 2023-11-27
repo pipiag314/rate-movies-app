6de223100039eededf72557f083e5c68f436897d
gela pipia <gelapipia10@gmail.com>
2023-11-27 23:01:09 +0400

    added navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <Menu fixed="top" size="huge">
+      <Menu.Menu position="right">
+        <Menu.Item as={Link} to="/auth">Auth</Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+};
+
+export default NavBar;
