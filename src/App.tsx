69621dfe8620f70ebce6cb3c267a0f926dbc5fd8
gela pipia <gelapipia10@gmail.com>
2023-11-29 01:00:07 +0400

    added Movie and TVShows dynamic routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import NavBar from "./components/NavBar";
 import "./App.css";
 import Auth from "./pages/auth";
 import Home from "./pages/home";
+import Movie from "./pages/movie";
+import TvShow from "./pages/tvshow";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/rated" element={<h1>your rated movies/tv shows</h1>} />
+          <Route path="/moive/:id" element={<Movie />} />
+          <Route path="/tvshow/:id" element={<TvShow />} />
         </Routes>
       </Router>
     </div>
