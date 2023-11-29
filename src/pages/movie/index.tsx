ee7fceb0f694d38574a634fc3b219799c03e8d98
gela pipia <gelapipia10@gmail.com>
2023-11-29 21:38:03 +0400

    updated movie/index.tsx: filled page content

--- src/pages/movie/index.tsx
+++ src/pages/movie/index.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Grid, Header, Loader, Segment, Image, List } from "semantic-ui-react";
+import { Grid, Header, Loader, Segment, Image, List, Label } from "semantic-ui-react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchMovieDetails } from "./query";
 import { prettyNumber } from "../../utils";
@@ -34,7 +34,7 @@ const Movie = () => {
                 style={{
                   display: "flex",
                   justifyContent: "center",
-                  alignItems: "center",
+                  alignItems: "flex-start",
                   height: "100%",
                 }}>
                     <Image size="medium" centered src={`https://image.tmdb.org/t/p/original/${data.poster_path}`} />
@@ -43,12 +43,46 @@ const Movie = () => {
             </Grid.Column>
             <Grid.Column width={10}>
                 <List>
-                    <List.Item>
-                        <List.Header>Only For Adults? {data.adult ? "Yes" : "No"}</List.Header>
-                    </List.Item>
-                    <List.Item>
-                        <List.Header>Budget: {prettyNumber(data.budget)}</List.Header>
-                    </List.Item>
+                  <List.Item>
+                    <List.Header>Only For Adults? {data.adult ? "Yes" : "No"}</List.Header>
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Budget: {prettyNumber(data.budget)}</List.Header>
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Genres: </List.Header>
+                    <div style={{display: "flex", gap: 10}}>
+                      {data?.genres?.map((genre: any) => (
+                        <Label key={genre.id}>{genre.name}</Label>
+                      ))}
+                    </div>
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>IMDB: </List.Header>
+                    {data.imdb_id}
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Popularity: </List.Header>
+                    {data.popularity}
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Revenue: </List.Header>
+                    {prettyNumber(data.revenue)}
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Runtime: </List.Header>
+                    {data.runtime + "min"}
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Vote: </List.Header>
+                    {data.vote_average}
+                  </List.Item>
+                  <List.Item>
+                    <List.Header>Language: </List.Header>
+                    {data.spoken_languages?.map((lang: any, index: number) => (
+                      <List.Item key={index}>{lang.name}</List.Item>
+                    ))}
+                  </List.Item>
                 </List>
             </Grid.Column>
           </Grid.Row>
