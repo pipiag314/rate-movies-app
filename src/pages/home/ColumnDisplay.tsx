16fa523290613dd175c76f43fce1a2db93359b26
gela pipia <gelapipia10@gmail.com>
2023-11-30 19:32:05 +0400

    added rating system: user is able to rate movie/tvshow

--- src/pages/home/ColumnDisplay.tsx
+++ src/pages/home/ColumnDisplay.tsx
@@ -1,6 +1,11 @@
 import { DisplayType } from "./index";
-import { Card, Grid } from "semantic-ui-react";
+import { Card, Grid, Form } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { rateMovie, rateTvShow } from "./mutation";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface DisplayData {
   id: number;
@@ -18,6 +23,36 @@ interface ColumnDisplayProps {
 }
 
 const ColumnDisplay = ({ dataType, data }: ColumnDisplayProps) => {
+  const [rating, setRating] = useState(0);
+
+  const notifyOnSuccess = (item: any) =>
+    toast.success(
+      `${
+        dataType === DisplayType.Movies ? item.title : item.name
+      } is succesfully rated!`
+    );
+  const notifyOnError = (item: any) =>
+    toast.error(
+      `${
+        dataType === DisplayType.Movies ? item.title : item.name
+      } is NOT rated! enter the number first`
+    );
+  
+
+  const { mutate: rateMovieMutation } = useMutation({
+    mutationKey: ["rateMovie"],
+    mutationFn: (id: number) => rateMovie(id, rating),
+  });
+  const { mutate: rateTvShowMutation } = useMutation({
+    mutationKey: ["rateTvShow"],
+    mutationFn: (id: number) => rateTvShow(id, rating),
+  });
+
+  const rateType =
+    dataType === DisplayType.Movies ? rateMovieMutation : rateTvShowMutation;
+
+  
+
   return (
     <Grid
       columns={3}
@@ -28,15 +63,47 @@ const ColumnDisplay = ({ dataType, data }: ColumnDisplayProps) => {
       {data?.map((item) => (
         <Grid.Column key={item.id}>
           <Card.Group>
-            <Link to={dataType === DisplayType.Movies ? `/moive/${item.id}` : `/tvshow/${item.id}`}>
+            <Link
+              to={
+                dataType === DisplayType.Movies
+                  ? `/moive/${item.id}`
+                  : `/tvshow/${item.id}`
+              }>
               <Card
                 fluid
                 image={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
-                header={dataType === DisplayType.Movies ? item.title : item.name}
+                header={
+                  dataType === DisplayType.Movies ? item.title : item.name
+                }
                 meta={`Release: ${item.release_data} | Rating: ${item.vote_average}`}
                 description={item.overview.slice(0, 300) + "..."}
               />
             </Link>
+            <Form>
+              <Form.Group>
+                <Form.Field>
+                  <Form.Input
+                    type="number"
+                    min={0}
+                    max={10}
+                    step={0.5}
+                    onChange={(e) => setRating(+e.target.value)}
+                    action={{
+                      color: "olive",
+                      icon: "star",
+                      onClick: () => {
+                        if(rating > 0) {
+                          rateType(item.id);
+                          notifyOnSuccess(item);
+                        } else {
+                          notifyOnError(item);
+                        }
+                      },
+                    }}
+                  />
+                </Form.Field>
+              </Form.Group>
+            </Form>
           </Card.Group>
         </Grid.Column>
       ))}
